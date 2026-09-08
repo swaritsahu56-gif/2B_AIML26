ae6471c30e0ca2e0282087238f6141d37f2c0688
User <user@example.com>
2026-09-08 12:09:34 +0530

    new

---
+++ objectlitreals.js
@@ -0,0 +1,18 @@
+// let name = "John";
+// let rollno = 12345;
+// let data={
+//     name,
+//     rollno
+// }
+// console.log(data);
+// console.log(data.name);
+// console.log(data.rollno);
+let firstname = "firstname";
+let lastname = "lastname";
+
+let data1 = {
+    [firstname]: "John",
+    [lastname]: "Doe"
+}
+console.log(data1.firstname);
+console.log(data1.lastname);
